3e6ee00c7721903122c17e60e49d6c9504a370c7
李涵祺 <02393963@yto.net.cn>
2022-11-04 13:42:18 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+const { defineConfig } = require('eslint-define-config')
+module.exports = defineConfig({
+  parser: 'vue-eslint-parser', // https://stackoverflow.com/questions/66597732/eslint-vue-3-parsing-error-expected-eslint
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  // files: ['*.ts', '*.vue'], // beta
+  root: true,
+  plugins: ['@typescript-eslint', 'prettier'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser', // 解决引入 type {} 报错问题
+    ecmaVersion: 2020, // 允许解析现代 ECMAScript
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true
+    }
+  },
+  rules: {
+    // 关闭名称校验 https://eslint.vuejs.org/rules/multi-word-component-names.html
+    'vue/multi-word-component-names': 'off',
+    // 禁止在模板中使用 this https://eslint.vuejs.org/rules/this-in-template.html
+    'vue/this-in-template': 'error',
+    // 禁止使用其他 lang https://eslint.vuejs.org/rules/block-lang.html
+    'vue/block-lang': [
+      'error',
+      {
+        script: {
+          lang: 'ts'
+        }
+      }
+    ],
+    // 禁止使用 v-html 来防止 XSS 攻击 https://eslint.vuejs.org/rules/no-v-html.html
+    'vue/no-v-html': 'error',
+    // 不允许修改 props https://eslint.vuejs.org/rules/no-mutating-props.html
+    'vue/no-mutating-props': 'error'
+  }
+})
---
+++ PRETTIER.md
@@ -0,0 +1,24 @@
+{
+//箭头函数永远包含括号
+"arrowParens": "always",
+// 将多行 HTML（HTML、JSX、Vue、Angular）元素的 > 放在最后一行的末尾，而不是单独放在下一行（不适用于自闭合元素）。
+"bracketSameLine": false,
+// 在对象文字的括号之间打印空格
+"bracketSpacing": true,
+// 控制 Prettier 是否格式化文件中嵌入的引用代码。
+"embeddedLanguageFormatting": "auto",
+// 指定 HTML、Vue、Angular 和 Handlebars 的全局空格敏感性。 有关详细信息，请参阅空格敏感格式。
+"htmlWhitespaceSensitivity": "css",
+// 在 JSX 中使用单引号而不是双引号
+"jsxSingleQuote": false,
+// 在语句的末尾打印分号
+"semi": false,
+// 使用单引号而不是双引号
+"singleQuote": true,
+// 缩进空格数
+"tabWidth": 2,
+//在多行逗号分隔的句法结构中尽可能打印尾随逗号。
+"trailingComma": "none",
+// 是否缩进 Vue 文件中的 <script> 和 <style> 标记内的代码
+"vueIndentScriptAndStyle": true
+}
